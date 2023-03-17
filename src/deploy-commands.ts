38ee642ad3be338b5f77808d46f8dac71c90a1ec
F.S. Koulen <feryllia.koulen@hva.nl>
2023-03-17 15:28:36 +0100

    Fix logging refresh status

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,6 +1,6 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 import { config } from 'dotenv'
-import { REST, Routes, type SlashCommandBuilder } from 'discord.js'
+import { REST, type RouteLike, Routes, type SlashCommandBuilder } from 'discord.js'
 import fs from 'node:fs'
 import path from 'node:path'
 
@@ -16,29 +16,27 @@ for (const file of commandFiles) {
 	const command = require(`./commands/${file}`)
 	commands.push(command.data.toJSON())
 }
-
-console.log(commands)
-
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN ?? '')
+
+// Get route to deploy based on environment
+function getRoute(environment: string): RouteLike {
+	if (environment === 'prod') {
+		return Routes.applicationCommands(process.env.CLIENT_ID ?? '')
+	}
+
+	return Routes.applicationGuildCommands(process.env.CLIENT_ID ?? '', process.env.GUILD_ID ?? '')
+}
 
 // and deploy your commands!
 void (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`)
-		let data: any
 		// The put method is used to fully refresh all commands in the guild with the current set
-		if (process.env.ENVIRONMENT === 'dev') {
-			data = await rest.put(
-				Routes.applicationGuildCommands(process.env.CLIENT_ID ?? '', process.env.GUILD_ID ?? ''),
-				{ body: commands }
-			)
-		} else if (process.env.ENVIRONMENT === 'prod') {
-			data = await rest.put(
-				Routes.applicationCommands(process.env.CLIENT_ID ?? ''),
-				{ body: commands }
-			)
-		}
+		const data: any = await rest.put(
+			getRoute(process.env.ENVIRONMENT ?? ''),
+			{ body: commands }
+		)
 		// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`)
 		// console.log(typeof data)
